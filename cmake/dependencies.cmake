be8d57dceffca7f157d9753d2d600147959ed7ef
Maximilian Stocker <max.stocker@gmx.de>
2023-05-06 22:00:27 +0200

    added vcpkg.json file, added fmt library

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,14 +1,5 @@
 # raylib
-find_package(raylib QUIET)
-if (NOT raylib_FOUND)
-    include(FetchContent)
-    FetchContent_Declare(
-        raylib
-        GIT_REPOSITORY https://github.com/raysan5/raylib.git
-        GIT_TAG 4.5.0
-    )
-    FetchContent_MakeAvailable(raylib)
-endif()
+find_package(raylib CONFIG REQUIRED)
 
 # raylib-cpp
 find_package(raylib_cpp QUIET)
@@ -22,4 +13,5 @@ if (NOT raylib_cpp_FOUND)
     FetchContent_MakeAvailable(raylib_cpp)
 endif()
 
-
+#fmt
+find_package(fmt CONFIG REQUIRED)
