007b23da827bced448b9a15c55bf2c61de1929cc
Maximilian <max.stocker@gmx.de>
2023-05-29 21:16:53 +0200

    switched from sfml to jngl

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,3 @@
-#include <SFML/Graphics.hpp>
+#include <iostream>
 int main() {
-  sf::RenderWindow window(sf::VideoMode(640, 480), "Testwindow");
-  // Creating our shape.
-  sf::RectangleShape rectangle(sf::Vector2f(window.getSize().x / 2 , window.getSize().x / 2));
-  rectangle.setFillColor(sf::Color::Red);
-  rectangle.setPosition(320, 240);
-  rectangle.setOrigin(rectangle.getSize().x / 2, rectangle.getSize().y / 2);
-  while (window.isOpen()) {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
-        // Close window button clicked.
-        window.close();
-      }
-    }
-    window.clear(sf::Color::Black);
-    window.draw(rectangle); // Drawing our shape.
-    window.display();
-  }
 }
