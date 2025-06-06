a0a014c9493bb4dfc7d0b2ef445d3b90a35afdf6
Maximilian Stocker <max.stocker@gmx.de>
2025-06-06 09:01:05 +0200

    added basic moving functions for the ship

--- Polybius/main.cpp
+++ Polybius/main.cpp
@@ -1,13 +1,13 @@
 #include <pthread.h>
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
-#include <SFML/Graphics/Color.hpp>
-#include <SFML/Window/Keyboard.hpp>
-#include <SFML/Window/WindowEnums.hpp>
+#include <SFML/Window.hpp>
 #include <iostream>
  
 int main()
 {
+    //init section
+
     // Create the main window
     sf::RenderWindow window(sf::VideoMode({1920, 1080}), "Polybius", sf::State::Windowed);
  
@@ -15,11 +15,13 @@ int main()
     const sf::Texture texture("./assets/sprites/ship.png");
     sf::Sprite sprite(texture);
     sprite.setScale({4,4});
- 
-    // Create a graphical text to display
-     const sf::Font font("./assets/fonts/Polybius1981.ttf");
-     sf::Text text(font, "Hello SFML", 50);
-    text.setPosition({window.getSize().x / 2.f, 0});
+    sprite.setPosition({(window.getSize().x / 2.f) + 100, window.getSize().y / 2.f});
+    
+    //kinda keep that snippet for subliminal message. Problably outsource that into its own procedure
+     // Create a graphical text to display
+     //const sf::Font font("./assets/fonts/Polybius1981.ttf");
+     //sf::Text text(font, "Hello SFML", 50);
+     //text.setPosition({window.getSize().x / 2.f, 0});
  
     // Load a music to play
     // sf::Music music("nice_music.ogg");
@@ -36,9 +38,27 @@ int main()
             // Close window: exit
             if (event->is<sf::Event::Closed>())
                 window.close();
+                //todo need to implement a rotation matrix that rotates the ship arround window.getWindowSize().x / 2 and windowy.getWindowSize().y / 2.f
+                /*
+                 the rotation matrix is a 2x2 matrix that is defined by 
+                 [cos(θ) -sin(θ)
+                  sin(θ) cos(θ)]
+                 */
             else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
                 if (keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
                     window.close();
+                }
+                else if (keyPressed->scancode == sf::Keyboard::Scancode::Right) {
+                    sprite.move({2,0});
+                }
+                else if (keyPressed->scancode == sf::Keyboard::Scancode::Left) {
+                sprite.move({-2,0});
+                }
+                else if (keyPressed->scancode == sf::Keyboard::Scancode::Down) {
+                    sprite.move({0, 2});
+                }
+                else if (keyPressed->scancode == sf::Keyboard::Scancode::Up) {
+                    sprite.move({0, -2});
                 }
             }
         }
@@ -50,7 +70,7 @@ int main()
         window.draw(sprite);
  
         // Draw the string
-        window.draw(text);
+        //window.draw(text);
  
         // Update the window
         window.display();
