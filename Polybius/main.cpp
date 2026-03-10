ea8b5136fe9d296111bdbe442f7ad5ac061b9b81
Maximilian Stocker <max.stocker@gmx.de>
2026-03-10 11:20:38 +0100

    fixed the problem with constantly rotating the ship

--- Polybius/main.cpp
+++ Polybius/main.cpp
@@ -1,10 +1,8 @@
-#include <pthread.h>
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/System/Angle.hpp>
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Window.hpp>
-#include <cstdlib>
 #include <iostream>
 
 int main()
@@ -44,8 +42,6 @@ int main()
             // Center is x1 and y1
             sf::Vector2f slope({sprite.getPosition().x - center.x, sprite.getPosition().y - center.y});
             
-            
-            //due to the definition of Vector2f slope.x will calculate calculates y2 - y1 and slope.y calculates x2 - x1
             //double radius = (slope.y) / (slope.x); 
 
             // Close window: exit
@@ -70,17 +66,19 @@ int main()
                 sprite.move({-2,0});
                 }
                 else if (keyPressed->scancode == sf::Keyboard::Scancode::Down) {
-                    sprite.move({0, slope.y + 0.75f});
+                    sprite.move({0, slope.x + 0.75f});
+                    std::cout <<  slope.x + 0.75f << "\n";
 
                     // PROBLEM: it's currently rotating the sprite 180 degree every frame. I just need it once. need to figure it out how
                     // TODO: need to implement a if clause that checks if the sprite has already rotated once.
                     // Another idea is to get the number of every corner of the sprite and them allign them in the wanted position
-                    //sprite.rotate(sf::degrees(180));                 
+                    sprite.setRotation(sf::degrees(360));                 
                 }
                 else if (keyPressed->scancode == sf::Keyboard::Scancode::Up) {
                     sprite.move({0, -(slope.y + 0.75f)});
+                    std::cout <<  -(slope.y + 0.75f) << "\n";
                     //same goes here. need to place that if clause here as well.
-                    //sprite.rotate(sf::degrees(180));
+                    sprite.setRotation(sf::degrees(180));
                 }
             }
         }
